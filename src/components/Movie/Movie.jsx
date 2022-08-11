1b1bae3f320350416ccd3045b7d2ed1fd04d3224
Reyhan Muhammad Januar <reyhanmuhammadj@gmail.com>
2022-08-11 21:22:58 +0700

    added show Movielist

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Typography, Grid, Grow, Tooltip, Rating } from '@mui/material';
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
 
-export default function Movie() {
+export default function Movie({ movie, i }) {
+  console.log(movie, i);
+  const classes = useStyles();
   return (
-    <div>Movie</div>
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          <img
+            src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : 'https://www.fillmurray.com/200/300'}
+            alt={movie.title}
+            className={classes.image}
+          />
+
+          <Typography className={classes.title} varriant="h5">
+            {movie.title}
+          </Typography>
+          <Tooltip disableFocusListener title={`${movie.vote_average} / 10 `}>
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+              <div>
+                <h1><span className="text-red-400">Voted</span> by {movie.vote_count} 🤩 </h1>
+              </div>
+            </div>
+          </Tooltip>
+        </Link>
+      </Grow>
+    </Grid>
   );
 }
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,12 +2,34 @@ import React, { useState } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
+import MovieList from '../MovieList/MovieList';
 
 export default function Movies() {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="cener">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please searh for something else.
+        </Typography>
+      </Box>
+    );
+  }
+  if (error) return 'an error occured';
   return (
 
-    <div>Moviess</div>
+    <div>
+      <MovieList movies={data} />
+    </div>
   );
 }
