b6807cc35f400bb33415e12ca2a318cb47033a53
Reyhan Muhammad Januar <reyhanmuhammadj@gmail.com>
2022-08-11 14:12:25 +0700

    setup Reduxtoolki baseUrl

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+
+export default function Movies() {
+  return (
+    <div>Movies</div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './components/App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const theme = createTheme({});
 root.render(
-
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
 
